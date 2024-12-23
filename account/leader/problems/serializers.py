8610c2b4b653ec0a02bfb18cd861802da0c288f2
behruz <xoliqbediyevbehruz@gmail.com>
2024-12-23 17:12:06 +0500

    fix: account/leader/problems/problemsApiView is fixed

--- account/leader/problems/serializers.py
+++ account/leader/problems/serializers.py
@@ -3,9 +3,23 @@ from rest_framework import serializers
 from account import models
 
 
+class EmployeeSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField(method_name='get_full_name')
+
+    class Meta:
+        model = models.User
+        fields = [
+            'id', 'full_name', 'phone_number', 'profile_image',
+        ]
+
+    def get_full_name(self, obj):
+        return obj.full_name
+
+
 class LeaderProblemsSerializer(serializers.ModelSerializer):
     date = serializers.SerializerMethodField(method_name='get_date')
     time = serializers.SerializerMethodField(method_name='get_time')
+    employee = serializers.SerializerMethodField(method_name='get_employee')
 
     class Meta:
         model = models.Problem
@@ -18,3 +32,6 @@ class LeaderProblemsSerializer(serializers.ModelSerializer):
 
     def get_time(self, obj):
         return obj.created_at.time()
+
+    def get_employee(self, obj):
+        return EmployeeSerializer(obj.employee).data if obj.employee else None
